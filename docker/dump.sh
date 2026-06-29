2c9401dd55fb7cce525115ee2dec61fcdd580f73
Shree Krishna Yadav <srikri5462ydv@gmail.com>
2026-06-29 20:23:30 +0545

    feat(radio): add monogo dump command

--- docker/dump.sh
+++ docker/dump.sh
@@ -6,7 +6,7 @@ for f in *.json; do
   collection="${f%.json}"
   echo "Importing $collection..."
   mongoimport \
-    --uri="mongodb://mongo:mongo@localhost:27017/radio_mongo?authSource=admin" \
+    --uri="mongodb://mongo:mongo@localhost:27017/core?authSource=admin" \
     --collection="$collection" \
     --file="$f" \
     --jsonArray
